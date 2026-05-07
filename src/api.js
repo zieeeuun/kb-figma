abf77ebc2891d35354867c5eeb737af6cb599525
metaapple <hera@naver.com>
2026-05-07 18:40:59 +0900

    json-server url변경

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://ai-lovable-1.onrender.com',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://ai-figma-4.onrender.com',
   headers: {
     'Content-Type': 'application/json'
   }
